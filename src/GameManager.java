471ba2eab870387a79ef565526f4f4317db13121
Callie Mao <callie.mao@gmail.com>
2015-01-28 00:49:22 -0500

    Updated XMLParser to read in several subNodes. Created getters and setters.

--- src/GameManager.java
+++ src/GameManager.java
@@ -5,13 +5,11 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
-
 public class GameManager {
 
-	
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException{
+	public static void main(String[] args) throws ParserConfigurationException,
+			SAXException, IOException {
 		XMLParser parser = new XMLParser();
 		parser.parseXMLFile(new File("src/fire2.xml"));
-		
 	}
 }
--- src/XMLParser.java
+++ src/XMLParser.java
@@ -16,25 +16,18 @@ import java.util.Map;
  
 public class XMLParser {
 	private NodeList myNodeList;
-	private NodeList myGridSetupList;
-	//private Map<String, Integer> myNodeMap; //using a map prevents the use of if/switch statements
+	private NodeList myGridParamList;
+	private NodeList myCellParamList;
 
-	/*public XMLParser(File xmlFile) throws ParserConfigurationException,
-        SAXException, IOException {
-
-	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-    Document doc = dBuilder.parse(xmlFile);
-    
-    //input data into nodes
-    myNodeList = doc.getDocumentElement().getChildNodes();
-	}*/
+    private Map<String, String> myGridParamMap;
+    private Map<String, String> myCellParamMap;
 	
 	public void parseXMLFile(File xmlFile) throws ParserConfigurationException,
     SAXException, IOException {
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 	    Document doc = dBuilder.parse(xmlFile);
+	    doc.getDocumentElement().normalize();
 	    
 	    //input data into nodes
 	    myNodeList = doc.getDocumentElement().getChildNodes();
@@ -43,44 +36,97 @@ public class XMLParser {
 	
 	public void parseInitialTags(){
 		for (int i = 0; i < myNodeList.getLength(); i++) {
-			//<gridSetup tag>
+			//<gridParam tag>
              Node node = myNodeList.item(i);
-             if (node instanceof Element) {
-                 myGridSetupList = node.getChildNodes();                  
+             if (node instanceof Element) {  
+            	switch (node.getNodeName()) {
+     			case "gridParam":
+                    myGridParamList = node.getChildNodes(); //use instance instead of global?                  
+                    makeGridParamMap(); //somehow pass this in just once later on instead of calling makeParamMap each time
+                    //maybe just use one myParamList variable and continuously override it?
+     				break;
+     			case "cellParam":
+     				myCellParamList = node.getChildNodes();
+     				makeCellParamMap();
+     				break;  
              }
 		}
-		setupGrid();
+	}
 	}
 	
-	public Map<String, Integer> setupGrid(){
-		Map<String, Integer> grid = new HashMap<String, Integer>();
-		  for (int j = 0; j < myGridSetupList.getLength(); j++) {
-              Node cNode = myGridSetupList.item(j);
-              if (cNode instanceof Element) {
-                String content = cNode.getTextContent();
-                switch (cNode.getNodeName()) {
-                  case "simName":
-                    System.out.println(content);
-                    break;
-                  case "author":
-                 	 System.out.println(cNode.getNodeValue());
-                    break;           
-                }
-          }
-      }
-		return grid;
+	//below methods have duplicated code
+	//fix by passing in parameters 
+	//public HashMap<> makeParamMap(paramList)
+	//and set it equal to other values
+	private void makeGridParamMap(){
+		myGridParamMap = new HashMap<String, String>();
+		for (int j = 0; j < myGridParamList.getLength(); j++) {
+			Node gridParamNode = myGridParamList.item(j);
+           //   if (cNode instanceof Element) {
+			String paramName = gridParamNode.getNodeName();
+			String content = gridParamNode.getTextContent();
+			myGridParamMap.put(paramName,content);
+		}
+   
+		System.out.println("print gridparammap");
+		for (String string : myGridParamMap.keySet()){
+			System.out.print(string + ": ");
+			System.out.println(myGridParamMap.get(string));
+		}
 	}
+	
+	private void makeCellParamMap(){
+		myCellParamMap = new HashMap<String, String>();
+		for (int j = 0; j < myCellParamList.getLength(); j++) {
+			Node cellParamNode = myCellParamList.item(j);
+           //   if (cNode instanceof Element) {
+			String paramName = cellParamNode.getNodeName();
+			String content = cellParamNode.getTextContent();
+			myCellParamMap.put(paramName,content);
+		}
+		
+		System.out.println("print cellparammap");
+		for (String string : myCellParamMap.keySet()){
+			System.out.print(string + ": ");
+			System.out.println(myCellParamMap.get(string));
+		}
+	}
+	
+	public Map<String, String> getGridParamMap(){
+		return myGridParamMap;
+	}
+	
+	public Map<String, String> getCellParamMap(){
+		return myCellParamMap;
+	}
+	
+	
 }
-		//}
-//		System.out.println(myGridSetupList.getLength());
-/*
-		for (int i = 0 ; i < myGridSetupList.getLength(); i++){
-			String test = myGridSetupList.item(i).getChildNodes().item(0).getNodeValue();
-			System.out.println(test);
-		}*/
-	//}
-    
- //}
+	
+
+//http://stackoverflow.com/questions/978810/how-to-strip-whitespace-only-text-nodes-from-a-dom-before-serialization
+/*try {
+    DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+    Document doc = builder.parse(input);
+    doc.getDocumentElement().normalize();   //added in since the edit
+    NodeList nodd = doc.getElementsByTagName("grandparent");
+    for (int x = 0; x < nodd.getLength(); x++){
+        Node node = nodd.item(x);
+        NodeList nodes = node.getChildNodes();
+        for(int y = 0; y < nodes.getLength(); y++){
+            Node n = nodes.item(y);
+            System.out.println(n.getNodeName());
+        }
+    }
+}*/
+
+/*File fXmlFile = new File(filepath);
+DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+Document doc = dBuilder.parse(fXmlFile);
+doc.getDocumentElement().normalize();
+NodeList nList = doc.getElementsByTagName("info");*/
 	
 
    
