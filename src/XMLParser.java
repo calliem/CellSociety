15916388eb45d14a7bdc94e99746094c5e9d47d8
Callie Mao <callie.mao@gmail.com>
2015-01-27 19:55:17 -0500

    Fixed XMLParser error. XMLParser now prints out values using getTextContent() instead of getNoteValue()

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -11,14 +11,13 @@ import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Map;
  
 public class XMLParser {
 	private NodeList myNodeList;
+	private NodeList myNodeList2;
 	private NodeList myGridSetupList;
 	//private Map<String, Integer> myNodeMap; //using a map prevents the use of if/switch statements
-	
 
 	public XMLParser(File xmlFile) throws ParserConfigurationException,
         SAXException, IOException {
@@ -26,30 +25,50 @@ public class XMLParser {
 	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
     Document doc = dBuilder.parse(xmlFile);
+    
+    //input data into nodes
     myNodeList = doc.getDocumentElement().getChildNodes();
-
    // parseInitialObjects();
 	}
 	
-	//there is an error somewhere here
 	public void parseInitialTags(){
 		for (int i = 0; i < myNodeList.getLength(); i++) {
+			//<gridSetup tag>
              Node node = myNodeList.item(i);
-             if (node.getNodeType() == Node.ELEMENT_NODE) {
-	             if (node.getNodeName() == "gridSetup"){
-	            	 myGridSetupList = node.getChildNodes();
+             if (node instanceof Element) {
+                 NodeList childNodes = node.getChildNodes();    
+                 for (int j = 0; j < childNodes.getLength(); j++) {
+                     Node cNode = childNodes.item(j);
+
+                     //Identifying the child tag of gridSetup encountered. 
+                     if (cNode instanceof Element) {
+                       String content = cNode.getTextContent();
+                       switch (cNode.getNodeName()) {
+                         case "simName":
+                           System.out.println(content);
+                           break;
+                         case "author":
+                        	 System.out.println(cNode.getNodeValue());
+                           break;
+                    
+                       }
+
 	             }
              }
-		}
-		System.out.println(myGridSetupList.getLength());
-
-		for (int i = 0 ; i < myGridSetupList.getLength(); i++){
-			String test = myGridSetupList.item(i).getNodeValue();
-			System.out.println(test);
+             }
 		}
 	}
+}
+		//}
+//		System.out.println(myGridSetupList.getLength());
+/*
+		for (int i = 0 ; i < myGridSetupList.getLength(); i++){
+			String test = myGridSetupList.item(i).getChildNodes().item(0).getNodeValue();
+			System.out.println(test);
+		}*/
+	//}
     
- }
+ //}
 	
 
    
