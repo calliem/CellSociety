d73122611348ff67354d25dfcab8a5b0ec7f13f8
Le Qi <le.qi19@gmail.com>
2015-01-28 01:12:00 -0500

    Added UI Skeleton to CellSociety

--- src/CellSociety.java
+++ src/CellSociety.java
@@ -1,3 +1,87 @@
-public class CellSociety {
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class CellSociety extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+				
+		stage.setTitle("CellSociety");
+		
+		HBox titleBox = new HBox(10);		
+		titleBox.setAlignment(Pos.CENTER);
+		Text title = new Text("Cell Society");		
+        title.setFont(Font.font("Helvetica", FontWeight.NORMAL, 32));
+        titleBox.setPadding(new Insets(15, 25, 05, 25));
+		titleBox.getChildren().add(title);
+        
+		GridPane mainGrid = new GridPane();
+		mainGrid.setHgap(10);
+		mainGrid.setVgap(10);
+		
+		GridPane simGrid = new GridPane();
+		mainGrid.setAlignment(Pos.CENTER);
+		simGrid.setHgap(1);
+		simGrid.setVgap(1);
+        simGrid.setPadding(new Insets(0, 25, 5, 25));
+
+		for (int i = 0; i < 50; i++) {
+			for (int j = 0; j < 50; j++) {
+				Rectangle r = new Rectangle(5, 5, Color.CYAN);
+				simGrid.add(r, i, j);
+			}
+		}
+		
+		mainGrid.add(titleBox, 0, 0);
+		mainGrid.add(simGrid, 0, 1);
+		
+		Button playButton = new Button("Play");
+		Button pauseButton = new Button("Pause");
+		Button stepButton = new Button("Step");
+		
+		Label speedLabel = new Label("Speed: ");
+		TextField speedText = new TextField();
+		Button xmlButton = new Button("Upload XML");
+		
+		HBox topRow = new HBox(10);
+		HBox bottomRow = new HBox(10);
+		
+		topRow.setAlignment(Pos.CENTER);
+		topRow.getChildren().add(playButton);
+		topRow.getChildren().add(pauseButton);
+		topRow.getChildren().add(stepButton);
+		topRow.getChildren().add(xmlButton);
+		topRow.setPadding(new Insets(0, 25, 5, 25));
+		
+		bottomRow.setAlignment(Pos.CENTER);
+		bottomRow.getChildren().add(speedLabel);
+		bottomRow.getChildren().add(speedText);
+		bottomRow.setPadding(new Insets(0, 25, 25, 25));
+		
+		mainGrid.add(topRow, 0, 2);
+		mainGrid.add(bottomRow, 0, 3);
+		
+		Scene scene = new Scene(mainGrid);
+		stage.setScene(scene);
+		stage.show();
+	}
 
 }
