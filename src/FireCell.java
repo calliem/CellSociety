f8ce2dda47de04ade09ce31569003be4832dab3c
Callie Mao <callie.mao@gmail.com>
2015-02-01 12:16:05 -0500

    Removed hardcoded solution and added a private variable to TreeCell

--- src/FireCell.java
+++ src/FireCell.java
@@ -2,16 +2,25 @@ import javafx.scene.paint.Color;
 
 public class FireCell extends Cell{
 	
+	private static Color myColor;
+	
 	public FireCell(){
 		super();
+		setFill(myColor); //could we add this to the superclass somehow or make a new method to setfill that way we don't have duplicated code?
+		
+		
 		//test below. color is not a static variable and needs to be updated every time for this to work. This isn't ideal so we'll have to find alternatives
 		//i think this issue probably applies to every cell but not sure why it's only noticeable for firecell right now
-		setFill(Color.valueOf("orange"));
+	//	setFill(Color.valueOf("orange"));
 	}
 
 	public FireCell(Color color) {
+	
 		super();
-		setFill(color);
+		myColor = color;
+		setFill(myColor);
+		//super();
+	//	setFill(color);
 	}
 	public String toString(){
 		return "FireCell";
