422b0d18abdfc9aa15569e1550e37191cf798581
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-02-06 20:27:39 +0000

    Added Readme

--- Doxygen/html/search/all_7.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['halfneighbor',['HalfNeighbor',['../d3/dcc/class_half_neighbor.html',1,'HalfNeighbor'],['../d3/dcc/class_half_neighbor.html#aa096c7cac6307ecc11ff28a55b2d43ae',1,'HalfNeighbor.HalfNeighbor()']]],
-  ['halfneighbor_2ejava',['HalfNeighbor.java',['../db/d52/_half_neighbor_8java.html',1,'']]],
-  ['hexagonalneighbor',['HexagonalNeighbor',['../df/d6f/class_hexagonal_neighbor.html',1,'HexagonalNeighbor'],['../df/d6f/class_hexagonal_neighbor.html#a87cf882817ea9de0bd686224ff8f9bd9',1,'HexagonalNeighbor.HexagonalNeighbor()']]],
-  ['hexagonalneighbor_2ejava',['HexagonalNeighbor.java',['../dd/d19/_hexagonal_neighbor_8java.html',1,'']]]
-];
