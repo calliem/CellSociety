fcfab8c87c284009769e7fd1266e48fbd71ef76e
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-02-09 03:07:26 +0000

    Added Readme

--- Doxygen/html/search/all_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['leavecurrentpatch',['leaveCurrentPatch',['../db/dfe/class_agent_cell.html#acbc24a5b4b9a1e4ea2236e992e8e36e5',1,'AgentCell']]],
-  ['lifecontroller',['LifeController',['../d9/da2/class_life_controller.html',1,'LifeController'],['../d9/da2/class_life_controller.html#a0ada855619696373103bf743dee753a3',1,'LifeController.LifeController()']]],
-  ['lifecontroller_2ejava',['LifeController.java',['../de/d30/_life_controller_8java.html',1,'']]]
-];
--- Doxygen/html/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['agentcell_2ejava',['AgentCell.java',['../d5/dea/_agent_cell_8java.html',1,'']]],
-  ['antground_2ejava',['AntGround.java',['../da/d4e/_ant_ground_8java.html',1,'']]]
-];
