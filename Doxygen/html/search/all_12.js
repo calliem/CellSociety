44e5aebc4c11d1b4fe37d44a0589e04dcf36baa6
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-02-09 03:57:39 +0000

    Added Readme

--- Doxygen/html/search/all_12.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['watorcontroller',['WatorController',['../dc/da7/class_wator_controller.html',1,'WatorController'],['../dc/da7/class_wator_controller.html#af99a6be818beaa46dc4120328c45b298',1,'WatorController.WatorController()']]],
-  ['watorcontroller_2ejava',['WatorController.java',['../df/d46/_wator_controller_8java.html',1,'']]]
-];
--- Doxygen/html/search/functions_10.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['watorcontroller',['WatorController',['../dc/da7/class_wator_controller.html#af99a6be818beaa46dc4120328c45b298',1,'WatorController']]]
-];
